5c4c2d096d72739021e7e4b32834c289c4e87af4
Mike Decker <pookmish@gmail.com>
2017-03-14 15:18:27 -0700

    test

---
+++ .travis.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf $HOME/html/sites/all/modules/stanford/stanford_paragraph_types
+git clone -b 7.x-1.x https://github.com/SU-SWS/stanford_paragraph_types.git $HOME/html/sites/all/modules/stanford/stanford_paragraph_types
+drush @local en stanford_story_page -y
