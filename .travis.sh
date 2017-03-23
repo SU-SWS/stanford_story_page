d4cfa26b58ca18460f035db580f30bf6286cbb03
Mike Decker <pookmish@gmail.com>
2017-03-23 11:04:19 -0700

    removed script file no longer needed

--- .travis.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#export PATH="$HOME/.composer/vendor/bin:$PATH"
-#
-#rm -rf $HOME/html/sites/all/modules/stanford/stanford_paragraph_types
-#git clone -b 7.x-1.x https://github.com/SU-SWS/stanford_paragraph_types.git $HOME/html/sites/all/modules/stanford/stanford_paragraph_types
-#drush @local en stanford_story_page -y
